b11d93008cd6f73bf32a90e2ab2ad0273ee41f8a
Faizal Sidek <faizal.sidek@gmail.com>
2021-06-24 12:43:18 +0800

    Update VM spec

--- azure/virtualmachine/linux/main.tf
+++ azure/virtualmachine/linux/main.tf
@@ -1,164 +1,79 @@
 data "azurerm_virtual_network" "vnet_name" {
-  name                = "${var.vnet_name}"
-  resource_group_name = "${var.vnet_rg_name}"
+  name                = var.vnet_name
+  resource_group_name = var.vnet_rg_name
 } 
 
 data "azurerm_subnet" "vm_subnet" {
-  name                  = "${var.subnet_name}"
-  virtual_network_name  = "${var.vnet_name}"
-  resource_group_name   = "${var.vnet_rg_name}"
+  name                  = var.subnet_name
+  virtual_network_name  = var.vnet_name
+  resource_group_name   = var.vnet_rg_name
 }
 data "azurerm_resource_group" "main" {
-  name     = "${var.rg_name}"
+  name     = var.rg_name
 }
 
 resource "azurerm_network_security_group" "nic_secgroup" {
-  count               = "${var.vm_count}"
+  count               = var.vm_count
   name                = "${format("nsg-%s%02.0f", var.vm_name, abs(count.index + 1))}"
-  location            = "${data.azurerm_resource_group.main.location}"
-  resource_group_name = "${data.azurerm_resource_group.main.name}"
-  tags                = "${var.tags}"
-
-/*
-  security_rule {
-    name                        = "AllowVnetInBound"
-    protocol                    = "*"
-    source_port_range           = "*"
-    destination_port_range      = "*"
-    source_address_prefix       = "VirtualNetwork"
-    destination_address_prefix  = "VirtualNetwork"
-    access                      = "Allow"
-    priority                    = "3000"
-    direction                   = "Inbound"
-  }
-
-  security_rule {
-    name                        = "AllowAzureLoadBalancerInBound"
-    protocol                    = "*"
-    source_port_range           = "*"
-    destination_port_range      = "*"
-    source_address_prefix       = "AzureLoadBalancer"
-    destination_address_prefix  = "VirtualNetwork"
-    access                      = "Allow"
-    priority                    = "3001"
-    direction                   = "Inbound"
-  }
-
-  security_rule {
-    name                        = "DenyAllInBound"
-    protocol                    = "*"
-    source_port_range           = "*"
-    destination_port_range      = "*"
-    source_address_prefix       = "*"
-    destination_address_prefix  = "*"
-    access                      = "Deny"
-    priority                    = "3500"
-    direction                   = "Inbound"
-  }
-
-  security_rule {
-    name                        = "AllowVnetOutBound"
-    protocol                    = "*"
-    source_port_range           = "*"
-    destination_port_range      = "*"
-    source_address_prefix       = "VirtualNetwork"
-    destination_address_prefix  = "VirtualNetwork"
-    access                      = "Allow"
-    priority                    = "3000"
-    direction                   = "Outbound"
-  }
-
-  security_rule {
-    name                        = "AllowInternetOutBound"
-    protocol                    = "*"
-    source_port_range           = "*"
-    destination_port_range      = "*"
-    source_address_prefix       = "*"
-    destination_address_prefix  = "Internet"
-    access                      = "Allow"
-    priority                    = "3001"
-    direction                   = "Outbound"
-  }
-
-  security_rule {
-    name                        = "DenyAllOutBound"
-    protocol                    = "*"
-    source_port_range           = "*"
-    destination_port_range      = "*"
-    source_address_prefix       = "*"
-    destination_address_prefix  = "*"
-    access                      = "Deny"
-    priority                    = "3500"
-    direction                   = "Outbound"
-  }
-*/
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  tags                = var.tags
 }
-/*
-resource "azurerm_public_ip" "vm-vip" {
-  count               = "${var.vm_count}"
-  name                = "${format("vip-%s%02.0f", var.vm_name, abs(count.index + 1))}"
-  location            = "${var.location}"
-  resource_group_name = "${data.azurerm_resource_group.main.name}"
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-*/
+
 resource "azurerm_network_interface" "main" {
-  count                     = "${var.vm_count}"
+  count                     = var.vm_count
   name                      = "${format("nic-%s%02.0f", var.vm_name, abs(count.index + 1))}"
-  location                  = "${data.azurerm_resource_group.main.location}"
-  resource_group_name       = "${data.azurerm_resource_group.main.name}"
-  #network_security_group_id = "${azurerm_network_security_group.nic_secgroup.*.id[count.index]}"
-  enable_ip_forwarding      = "true"
-  tags                      = "${var.tags}"
+  location                  = data.azurerm_resource_group.main.location
+  resource_group_name       = data.azurerm_resource_group.main.name
+  enable_ip_forwarding      = true
+  tags                      = var.tags
 
   ip_configuration {
     name                          = "${format("ipconfig-%s%02.0f", var.vm_name, abs(count.index + 1))}"
-    subnet_id                     = "${data.azurerm_subnet.vm_subnet.id}"
+    subnet_id                     = data.azurerm_subnet.vm_subnet.id
     private_ip_address_allocation = "Dynamic"
-    // public_ip_address_id          = "${azurerm_public_ip.vm-vip.*.id[count.index]}"
   }
 }
 
 output "nic_ids" {
-  value = "${azurerm_network_interface.main.*.id}"
+  value = azurerm_network_interface.main.*.id
 }
 
 resource "azurerm_availability_set" "main" {
-  name                        = "${var.vm_name}"
-  location                    = "${data.azurerm_resource_group.main.location}"
-  resource_group_name         = "${data.azurerm_resource_group.main.name}"
+  name                        = var.vm_name
+  location                    = data.azurerm_resource_group.main.location
+  resource_group_name         = data.azurerm_resource_group.main.name
   platform_fault_domain_count = 2
-  tags                        = "${var.tags}"
+  tags                        = var.tags
 }
 
 resource "azurerm_virtual_machine" "main" {
-  count                         = "${var.vm_count}"
+  count                         = var.vm_count
   name                          = "${format("%s%02.0f", var.vm_name, abs(count.index + 1))}"
-  location                      = "${data.azurerm_resource_group.main.location}"
-  resource_group_name           = "${data.azurerm_resource_group.main.name}"
-  network_interface_ids         = ["${azurerm_network_interface.main.*.id[count.index]}"]
-  vm_size                       = "${var.vm_size}"
+  location                      = data.azurerm_resource_group.main.location
+  resource_group_name           = data.azurerm_resource_group.main.name
+  network_interface_ids         = [azurerm_network_interface.main.*.id[count.index]]
+  vm_size                       = var.vm_size
   delete_os_disk_on_termination = true
-  tags                          = "${var.tags}"
-  availability_set_id           = "${azurerm_availability_set.main.id}"
+  tags                          = var.tags
+  availability_set_id           = azurerm_availability_set.main.id
 
   storage_image_reference {
-    publisher = "${var.vm_image["publisher"]}"
-    offer     = "${var.vm_image["offer"]}"
-    sku       = "${var.vm_image["sku"]}"
-    version   = "${var.vm_image["version"]}"
+    publisher = var.vm_image["publisher"]
+    offer     = var.vm_image["offer"]
+    sku       = var.vm_image["sku"]
+    version   = var.vm_image["version"]
   }
 
   os_profile {
     computer_name   = "${format("%s%02.0f", var.vm_name, abs(count.index + 1))}"
-    admin_username  = "${var.admin_username}"
+    admin_username  = var.admin_username
   }
 
   os_profile_linux_config {
     disable_password_authentication = true
     ssh_keys {
-      key_data  = "${var.ssh_file_content}"
+      key_data  = var.ssh_file_content
       path      = "/home/${var.admin_username}/.ssh/authorized_keys"
     }
   }
@@ -167,23 +82,31 @@ resource "azurerm_virtual_machine" "main" {
     name          = "${format("disk-%s%02.0f-OS", var.vm_name, abs(count.index + 1))}"
     create_option = "FromImage"
   }
+
+  dynamic "identity" {
+    for_each = var.vm_ids
+    content {
+      type          = "UserAssigned"
+      identity_ids  = [identity.value]
+    }
+  }
 }
 
 resource "azurerm_managed_disk" "data_disk" {
-  count                 = "${var.vm_count}"
+  count                 = var.vm_count
   name                  = "${format("disk-%s%02.0f-data", var.vm_name, abs(count.index + 1))}"
-  resource_group_name   = "${data.azurerm_resource_group.main.name}"
-  location              = "${data.azurerm_resource_group.main.location}"
+  resource_group_name   = data.azurerm_resource_group.main.name
+  location              = data.azurerm_resource_group.main.location
   storage_account_type  = "Standard_LRS"
   create_option         = "Empty"
-  disk_size_gb          = "${var.vm_datadisk_size_gb}"
-  tags                  = "${var.tags}"
+  disk_size_gb          = var.vm_datadisk_size_gb
+  tags                  = var.tags
 }
 
 resource "azurerm_virtual_machine_data_disk_attachment" "attach-datadisk" {
-  count               = "${var.vm_count}"
-  managed_disk_id     = "${azurerm_managed_disk.data_disk.*.id[count.index]}"
-  virtual_machine_id  = "${azurerm_virtual_machine.main.*.id[count.index]}"
+  count               = var.vm_count
+  managed_disk_id     = azurerm_managed_disk.data_disk.*.id[count.index]
+  virtual_machine_id  = azurerm_virtual_machine.main.*.id[count.index]
   lun                 = "${count.index + 1}"
   caching             = "ReadWrite"
 }
--- azure/virtualmachine/linux/variables.tf
+++ azure/virtualmachine/linux/variables.tf
@@ -7,7 +7,7 @@ variable "vm_name" {
 }
 variable "location" {}
 variable "tags" {
-  type  = "map"
+  type  = map
 }
 variable "vm_count" {}
 variable "vm_size" {
@@ -17,5 +17,8 @@ variable "ssh_file_content" {}
 variable "admin_username" {}
 variable "vm_datadisk_size_gb" {}
 variable "vm_image" {
-  type  = "map"
+  type  = map
+}
+variable "vm_ids" {
+  type = list
 }
